41b2acbae75b08890831865e012d3dc7bdf7e8fd
martypdx <marty@martypdx.com>
2018-11-26 12:05:02 -0800

    basic list html and css

--- week01_view-with-vue/class01_intro-vuejs/demo/src/App.vue
+++ week01_view-with-vue/class01_intro-vuejs/demo/src/App.vue
@@ -1,13 +1,17 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
+    <Pirates/>
   </div>
 </template>
 
 <script>
+import Pirates from './components/Pirates.vue';
 
 export default {
-
+  components: {
+    // Pirates: Pirates
+    Pirates
+  }
 };
 </script>
 
@@ -16,8 +20,6 @@ export default {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
---
+++ week01_view-with-vue/class01_intro-vuejs/demo/src/components/Pirates.vue
@@ -0,0 +1,39 @@
+<template>
+    <ul>
+        <li>
+            <h3>Monkey D Luffy the Captain</h3>
+            <img src="https://upload.wikimedia.org/wikipedia/en/c/cb/Monkey_D_Luffy.png">
+        </li>
+    </ul>
+</template>
+
+<script>
+export default {
+
+};
+</script>
+
+<style>
+li {
+    position: relative;
+    display: inline-block;
+    width: 200px;
+    height: 200px;
+    border: 1px solid #aaa;
+    text-align: center;
+}
+
+h3 {
+    text-shadow: 1px 1px 1px white;
+}
+
+img {
+    position: absolute;
+    top: 0; left: 0; right: 0;
+    margin-top: 10px;
+    height: 100%;
+    width: auto;
+    z-index: -1;
+    margin: auto;
+}
+</style>
