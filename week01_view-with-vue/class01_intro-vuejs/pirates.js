8d7b7d6d08431fe1a615f9fa1568d13491a1c1e0
martypdx <marty@martypdx.com>
2018-11-26 08:49:10 -0800

    class 01 setup

---
+++ week01_view-with-vue/class01_intro-vuejs/pirates.js
@@ -0,0 +1,22 @@
+const pirates = [
+  {
+    name: 'Monkey D. Luffy',
+    image: 'https://upload.wikimedia.org/wikipedia/en/c/cb/Monkey_D_Luffy.png',
+    role: 'captain',
+    crew: 'Straw Hats'
+  },
+  {
+    name: 'Ronona Zoro',
+    image: 'https://vignette.wikia.nocookie.net/p__/images/5/50/Roronoa_Zoro.png/revision/latest?cb=20151028103531&path-prefix=protagonist',
+    role: 'swordsperson',
+    crew: 'Straw Hats'
+  },
+  {
+    name: 'Nami',
+    image: 'https://vignette.wikia.nocookie.net/p__/images/8/84/Nami_Unlimited_World_Red_Post_Skip.png/revision/latest?cb=20140425151657&path-prefix=protagonist',
+    role: 'navigator',
+    crew: 'Straw Hats'
+  }
+];
+
+export default pirates;
