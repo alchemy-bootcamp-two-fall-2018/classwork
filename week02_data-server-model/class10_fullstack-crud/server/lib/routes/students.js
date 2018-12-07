d772a0a9e31b5f0d3ea7b6c764c7880351f6c598
martypdx <marty@martypdx.com>
2018-12-07 09:44:54 -0800

    move student routes to own router file (module)

---
+++ week02_data-server-model/class10_fullstack-crud/server/lib/routes/students.js
@@ -0,0 +1,66 @@
+const express = require('express');
+const client = require('../../db-client');
+const Router = express.Router;
+const router = Router(); //eslint-disable-line new-cap
+
+router
+  .get('/api/students', (req, res) => {
+    client.query(`
+      SELECT 
+        student.id, 
+        student.name as name,
+        start_date as "startDate",
+        track.id as "trackId",  
+        track.name as track
+      FROM student
+      JOIN track
+      ON student.track_id = track.id
+      ORDER BY start_date DESC, name ASC;
+    `)
+      .then(result => {
+        res.json(result.rows);
+      });
+  })
+
+  .get('/api/students/:id', (req, res) => {
+    client.query(`
+      SELECT * FROM student WHERE id = $1;
+    `,
+    [req.params.id])
+      .then(result => {
+        res.json(result.rows[0]);
+      });
+  })
+
+  .post('/api/students', (req, res) => {
+    const body = req.body;
+
+    client.query(`
+      INSERT INTO student (name, track_id, start_date)
+      VALUES($1, $2, $3)
+      RETURNING id;
+    `,
+    [body.name, body.trackId, body.startDate])
+      .then(result => {
+        const id = result.rows[0].id;
+        
+        return client.query(`
+          SELECT 
+            student.id, 
+            student.name as name,
+            start_date as "startDate",
+            track.id as "trackId",  
+            track.name as track
+          FROM student
+          JOIN track
+          ON student.track_id = track.id 
+          WHERE student.id = $1;
+        `,
+        [id]);
+      })
+      .then(result => {
+        res.json(result.rows[0]);
+      });
+  });
+
+module.exports = router;
--- week02_data-server-model/class10_fullstack-crud/server/server.js
+++ week02_data-server-model/class10_fullstack-crud/server/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const morgan = require('morgan');
+const students = require('./lib/routes/students');
 const client = require('./db-client');
 
 // enhanced logging
@@ -24,65 +25,8 @@ app.get('/api/tracks', (req, res) => {
     });
 });
 
+app.use(students);
 
-app.get('/api/students', (req, res) => {
-  client.query(`
-    SELECT 
-      student.id, 
-      student.name as name,
-      start_date as "startDate",
-      track.id as "trackId",  
-      track.name as track
-    FROM student
-    JOIN track
-    ON student.track_id = track.id
-    ORDER BY start_date DESC, name ASC;
-  `)
-    .then(result => {
-      res.json(result.rows);
-    });
-});
-
-app.get('/api/students/:id', (req, res) => {
-  client.query(`
-    SELECT * FROM student WHERE id = $1;
-  `,
-  [req.params.id])
-    .then(result => {
-      res.json(result.rows[0]);
-    });
-});
-
-app.post('/api/students', (req, res) => {
-  const body = req.body;
-
-  client.query(`
-    INSERT INTO student (name, track_id, start_date)
-    VALUES($1, $2, $3)
-    RETURNING id;
-  `,
-  [body.name, body.trackId, body.startDate])
-    .then(result => {
-      const id = result.rows[0].id;
-      
-      return client.query(`
-        SELECT 
-          student.id, 
-          student.name as name,
-          start_date as "startDate",
-          track.id as "trackId",  
-          track.name as track
-        FROM student
-        JOIN track
-        ON student.track_id = track.id 
-        WHERE student.id = $1;
-      `,
-      [id]);
-    })
-    .then(result => {
-      res.json(result.rows[0]);
-    });
-});
 
 /* end defined routes */
 
