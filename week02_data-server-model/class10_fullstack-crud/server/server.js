5b2e490668af350181d7d4db53ee6fc0bb89eb67
martypdx <marty@martypdx.com>
2018-12-07 09:53:29 -0800

    split server.js into app part with setup logic, and server part for starting server

--- week02_data-server-model/class10_fullstack-crud/server/server.js
+++ week02_data-server-model/class10_fullstack-crud/server/server.js
@@ -1,30 +1,8 @@
-const express = require('express');
-const app = express();
-const morgan = require('morgan');
-const students = require('./lib/routes/students');
-const tracks = require('./lib/routes/tracks');
+const app = require('./lib/app');
 
-// enhanced logging
-app.use(morgan('dev'));
-
-// register the json "middleware" body parser
-app.use(express.json());
-
-/* Defined routes: METHOD, URL PATH */
-// method == app.<method>
-// path = app.get('/this/is/path', ...)
-
-app.use('/api/tracks', tracks);
-app.use('/api/students', students);
-
-
-/* end defined routes */
-
-/* configure and start the server */
 const PORT = 3000;
 
 app.listen(PORT, () => {
   console.log('server app started on port', PORT);
 });
 
-/* end configure and server start */
