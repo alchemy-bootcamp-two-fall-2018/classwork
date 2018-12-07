cf2626f8a2916b0813aeeb9c17e3475a70b18628
martypdx <marty@martypdx.com>
2018-12-07 09:47:42 -0800

    actually use tracks router

--- week02_data-server-model/class10_fullstack-crud/server/server.js
+++ week02_data-server-model/class10_fullstack-crud/server/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const morgan = require('morgan');
 const students = require('./lib/routes/students');
-const client = require('./db-client');
+const tracks = require('./lib/routes/tracks');
 
 // enhanced logging
 app.use(morgan('dev'));
@@ -14,17 +14,7 @@ app.use(express.json());
 // method == app.<method>
 // path = app.get('/this/is/path', ...)
 
-app.get('/api/tracks', (req, res) => {
-  client.query(`
-    SELECT id, name, short_name as "shortName"
-    FROM track
-    ORDER BY name;
-  `)
-    .then(result => {
-      res.json(result.rows);
-    });
-});
-
+app.use(tracks);
 app.use(students);
 
 
